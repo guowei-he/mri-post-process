eff156fa01a4986ac927d38e76b4aba7747b091b
Guowei He <gh50@nyu.edu>
2017-12-06 14:33:03 +0400

    with input completeness check

--- mri-post-process.sh
+++ mri-post-process.sh
@@ -22,19 +22,44 @@ err() { echo "$@" 1>&2; exit 1; }
 outbase="/Users/user/Desktop/output"
 converter="/Users/user/Desktop/dcm2niix"
 
+check_input_size() {
+  local indir ntries seconds_between_tries previous_size current_size is_timeout
+  indir="$1"
+  ntries=20
+  seconds_between_tries=60
+  previous_size="-1"
+  is_timeout="true"
+  for i in $(seq 1 ${ntries}); do
+    sleep ${seconds_between_tries}
+    current_size=$(du -d 0 "${indir}" | awk '{print $1}')
+    if [[ "${current_size}" == "${previous_size}" ]]; then
+      is_timeout="false"
+      echo "Input is complete at trail ${i}. Input size: ${current_size}. $(date)"
+      break
+    fi
+    echo "Trail $i, input still not complete. ${current_size} vs ${previous_size}. $(date)"
+    previous_size="${current_size}"
+  done
+  if [[ "${is_timeout}" == "true" ]]; then
+    err "Input is not complete. Waited until $(date)."
+  fi
+}
+
 main() {
   # Check input
   if [[ "$#" -ne 1 ]]; then
     err "Usage: mri-post-process.sh dcm-folder"
   fi
-  in="$1"
-  base="$(basename ${in})"
-  if [[ ! -d "${in}" ]]; then
-    err "Error: Input ${in} does not exist."
+  indir="$1"
+  base="$(basename ${indir})"
+  if [[ ! -d "${indir}" ]]; then
+    err "Error: Input ${indir} does not exist."
   fi
-  if [[ ! "${in}" = /* ]]; then
-    err "Error: Input ${in} has to be absolute path."
+  if [[ ! "${indir}" = /* ]]; then
+    err "Error: Input ${indir} has to be absolute path."
   fi
+  # wait until the input folder is complete / size not changing
+  check_input_size "${indir}"
 
   # Check global variables
   if [[ ! -f "${converter}" ]]; then
@@ -43,21 +68,21 @@ main() {
   if [[ ! -d "${outbase}" ]]; then
     err "Error: Output base dir ${outbase} does not exist."
   fi
-  out="${outbase}/${base}"
-  if [[ -d "${out}" ]]; then
-    err "Error: Output dir ${out} already exists! Make sure you are not overwriting. If sure, delete it and continue."
+  outdir="${outbase}/${base}"
+  if [[ -d "${outdir}" ]]; then
+    err "Error: Output dir ${outdir} already exists! Make sure you are not overwriting. If sure, delete it and continue."
   else
-    mkdir -p ${out}
+    mkdir -p ${outdir}
   fi
 
-  if [[ ! -d "${out}" ]]; then
-    err "Error: Failed to create ${out}."
+  if [[ ! -d "${outdir}" ]]; then
+    err "Error: Failed to create ${outdir}."
   fi
 
   # Convert the file
-  cd ${in}
-  find ./ -name "*.IMA" | xargs -I {} dirname {} | sort -u | xargs -I {}  mkdir -p ${out}/{}
-  find ./ -name "*.IMA" | xargs -I {} dirname {} | sort -u | xargs -I {}  ${converter} -b y -z n -o ${out}/{} {}
+  cd ${indir}
+  find ./ -name "*.IMA" | xargs -I {} dirname {} | sort -u | xargs -I {}  mkdir -p ${outdir}/{}
+  find ./ -name "*.IMA" | xargs -I {} dirname {} | sort -u | xargs -I {}  ${converter} -b y -z i -o ${outdir}/{} {}
 }
 
 main "$@"
